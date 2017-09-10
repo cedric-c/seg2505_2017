f2ebea7f41eb7cceb612c96c73969e4f2432eae1
Cédric Clément <cclem054@uottawa.ca>
2017-09-09 20:48:32 -0400

    thebest is now better

--- labs/01_git/TheBest.java
+++ labs/01_git/TheBest.java
@@ -2,10 +2,6 @@
 public class TheBest {
     
     public static void main(String[] args) {
-        PersonInfo1.displayFr();
-        PersonInfo2.displayFr();
-        PersonInfo3.displayFr();
-        PersonInfo4.displayFr();
-	//PersonInfo5.displayFr();
+
     }
 }
